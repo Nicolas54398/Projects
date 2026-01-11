5900033d971849b8033ee7b15300386175d98cab
Nicolas54398 <c.sopheary@yahoo.com>
2026-01-11 11:31:45 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function tiger() {
+    document.getElementById("tiger-info").innerHTML =
+        "The tiger (Panthera tigris) is the largest extant cat species and a member of the genus Panthera.";
+}
+
+function lion() {
+    document.getElementById("lion-info").innerHTML =
+        "The lion (Panthera leo) is a species in the family Felidae and a member of the genus Panthera.";
+
+}
