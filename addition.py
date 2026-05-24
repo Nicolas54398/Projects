4cb815f36949dbaf9509bc9f5a8d0d4720c9b1ee
Nicolas54398 <c.sopheary@yahoo.com>
2026-05-24 10:46:47 +1000

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,5 @@
+# Addition Code
+num1 = 6
+num2 = 9
+sum = num1 + num2
+print("The sum is:", sum)
---
+++ multiplication.py
@@ -0,0 +1,7 @@
+# Multiplication Code
+    
+num2 = 6
+num3 = 9
+multiplication = num2 * num3
+
+print("The result is:", multiplication)
