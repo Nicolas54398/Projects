486b285a20d1f933ead92fec2a1c9b703ccc0430
Nicolas54398 <c.sopheary@yahoo.com>
2026-05-28 19:38:56 +1000

    Add files via upload

---
+++ coversation_strings.py
@@ -0,0 +1,19 @@
+person_1 = "David"
+person_2 = "Anna"
+david_1 = "Hello Anna how is your day?"
+anna_1 = "My day is great! How about yours?"
+david_2 = "My day is alright."
+anna_2 = "Is your grades high in education?"
+david_3 = "Yes it's very high in education!"
+anna_3 = "That's amazing!"
+david_4 = "Bye Anna!"
+anna_4 = "Bye David!"
+
+print(david_1)
+print(anna_1)
+print(david_2)
+print(anna_2)
+print(david_3)
+print(anna_3)
+print(david_4)
+print(anna_4)
