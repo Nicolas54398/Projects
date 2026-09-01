13a334238ad4a45466a5819b1891168339e68f29
Nicolas54398 <c.sopheary@yahoo.com>
2026-09-01 20:08:36 +1000

    Add files via upload

---
+++ value_key.py
@@ -0,0 +1,19 @@
+# Solve: a + 56 = 234
+answer = 234 - 56
+print("The value of a is:", answer)
+
+# Solve: b + 547 = 3465
+answer = 3465 - 547
+print("The value of b is:", answer)
+
+# Solve: c + 8625 = 23876
+answer = 23876 - 8625
+print("The value of c is:", answer)
+
+# Solve: d + 56336 = 102393
+answer = 102393 - 56336
+print("The value of d is:", answer)
+
+# Solve: e + 453452 = 865565
+answer = 865565 - 453452
+print("The value of e is:", answer)
